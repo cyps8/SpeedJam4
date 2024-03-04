90e04498f797b946891e38867f75ec4a1a29d65d
cyp <cypriane81@gmail.com>
2024-03-04 16:07:26 +0000

    [+Added] Credits

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+var creditsCanvas: CanvasLayer
+
+func _ready():
+	creditsCanvas = $Credits
+	remove_child(creditsCanvas)
+
 func StartButton():
 	SceneManager.instance.ChangeScene(SceneManager.Scene.GAME)
 
@@ -8,3 +14,9 @@ func OptionsButton():
 
 func QuitButton():
 	get_tree().quit()
+
+func CreditsButton():
+	add_child(creditsCanvas)
+
+func CreditsBackButton():
+	remove_child(creditsCanvas)
--- Scripts/Water.gd
+++ Scripts/Water.gd
@@ -1,14 +1,22 @@
 extends Area2D
 
+func _ready():
+	for waterfall in get_tree().get_nodes_in_group("Waterfall"):
+		waterfall.visible = false
+
 func Flood():
 	var tween = create_tween()
 	tween.tween_property(self, "position:y", 180, 2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	for waterfall in get_tree().get_nodes_in_group("Waterfall"):
+		waterfall.visible = true
 
 func RaiseWater():
 	var tween = create_tween()
 	tween.tween_property(self, "position:y", 615, 2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	print("water rising!")
 	%BackLabel.visible = true
+	for waterfall in get_tree().get_nodes_in_group("Waterfall"):
+		waterfall.visible = true
 
 func _process(_delta):
 	%WaterSprite.position.y = position.y - 506
